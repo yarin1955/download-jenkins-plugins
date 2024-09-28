2384d35d94a56e500ae30304e52d46343d70f0eb
yarin <yarins1955@gmail.com>
2024-09-28 16:41:38 +0300

    clean code

--- main.ps1
+++ main.ps1
@@ -1,12 +1,13 @@
 $plugins_list = @()
 
 $dependecies_plugins= New-Object System.Collections.Generic.HashSet[object];
-# Send a GET request
-$data = Invoke-WebRequest -Uri "https://updates.jenkins.io/update-center.actual.json?version=2.361.4" | ConvertFrom-Json
 
 $vulnerability_plugins= New-Object System.Collections.Generic.HashSet[object];
 
-Function get_vulnerability_plugins {
+$data = Invoke-WebRequest -Uri "https://updates.jenkins.io/update-center.actual.json?version=2.361.4" | ConvertFrom-Json
+
+
+function get_vulnerability_plugins {
     foreach ($plugin in $data.warnings) {
         if($plugin.type -eq "plugin") {
             #$unsecure += $plugin.name
@@ -15,19 +16,18 @@ Function get_vulnerability_plugins {
     }
 }
 
-Function get_plugins {
+function get_plugins {
     foreach ($key in $data.plugins.PSObject.Properties.Name) {
         $plugin = $data.plugins."$key"
 
         $script:plugins_list += [PSCustomObject]@{  name = $plugin.name; version = $plugin.version }
 
-        #[void]$Script:dependecies_plugins.Add(($plugin.dependencies | Where-Object { -not $_.optional} | Select-Object name, optional))
         [void]$Script:dependecies_plugins.Add($plugin.dependencies)
 
     }
 }
 
-Function remove_dependency_from_vulnerability_plugins() {
+function remove_dependency_from_vulnerability_plugins() {
     foreach ($item in $script:dependecies_plugins) {
         if($item.optional) {
             [void]$script:vulnerability_plugins.Remove($item.name)
@@ -42,10 +42,6 @@ get_vulnerability_plugins
 remove_dependency_from_vulnerability_plugins
 
 $plugins_list = $plugins_list | Where-Object { -not $vulnerability_plugins.Contains($_.name) }
-
-
-$plugins_list
-#Measure-Command { .\Untitled3.ps1 }
 
 
 
